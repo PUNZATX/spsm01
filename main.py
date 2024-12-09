3e15a6838d7687a228da5303a0fc078061c27c16
PUNZATX <dinotv586@gmail.com>
2024-12-09 11:09:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+password = "spsm"
+
+input_password = input("Please enter your password: ")
+
+if password == input_password :
+    print("Correct")
+
+else:
+    print("Wrong password")
